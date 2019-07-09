efacf6976e32ca055bc2f3a5b98d15e8b6144e55
Elijah <elijahs2106@gmail.com>
2019-07-09 14:50:26 -0400

    Finish exercise 2.11

--- phonebook/src/Persons.js
+++ phonebook/src/Persons.js
@@ -4,7 +4,7 @@ import Person from './Person'
 
 const Persons = ({persons, filter}) => {
   const loadPersons = () => persons
-    .filter((person) => person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1 || person.number.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+    .filter((person) => person.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1 || person.number.toLowerCase().indexOf(filter.toLowerCase()) !== 1)
     .map((person) => <Person key={person.name} name={person.name} number={person.number} />)
 
   return (
